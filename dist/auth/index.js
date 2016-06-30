e33c7f1516663adca214cbe601429c06b72ad048
cablej <jackhcable@gmail.com>
2016-06-30 10:41:39 -0500

    Update overwriting views

--- dist/auth/index.js
+++ dist/auth/index.js
@@ -33,48 +33,57 @@ require('./views/password.js');
 require('./views/reset.js');
 require('./views/signup.js');
 
+app.config(function ($stateProvider) {
+  $stateProvider.state('login', {
+    url: '/login?referrer',
+    templateUrl: 'authApp/auth/views/login.html',
+    controller: 'LoginController',
+    controllerAs: 'vm',
+    parent: 'app'
+  }).state('logout', {
+    url: '/logout',
+    templateUrl: 'authApp/auth/views/login.html',
+    parent: 'app',
+    controller: function controller($state, Auth) {
+      Auth.logout();
+      $state.go('login');
+    }
+  }).state('signup', {
+    url: '/signup?referrer&r',
+    templateUrl: 'authApp/auth/views/signup.html',
+    controller: 'SignupController',
+    controllerAs: 'vm',
+    parent: 'app'
+  }).state('password', {
+    url: '/password',
+    templateUrl: 'authApp/auth/views/password.html',
+    controller: 'PasswordController',
+    controllerAs: 'vm',
+    authenticate: true,
+    parent: 'app'
+  }).state('forgot', {
+    url: '/forgot',
+    templateUrl: 'authApp/auth/views/forgot.html',
+    controller: 'ForgotController',
+    controllerAs: 'vm',
+    parent: 'app'
+  }).state('reset', {
+    url: '/reset/:token',
+    templateUrl: 'authApp/auth/views/reset.html',
+    controller: 'ResetController',
+    controllerAs: 'vm',
+    parent: 'app'
+  });
+});
+
 function setOverwriteViews(views) {
-  app.config(function ($stateProvider) {
-    $stateProvider.state('login', {
-      url: '/login?referrer',
-      templateUrl: (views.login ? 'app' : 'authApp') + '/auth/views/login.html',
-      controller: 'LoginController',
-      controllerAs: 'vm',
-      parent: 'app'
-    }).state('logout', {
-      url: '/logout',
-      templateUrl: 'app/auth/views/login.html',
-      parent: 'app',
-      controller: function controller($state, Auth) {
-        Auth.logout();
-        $state.go('login');
+  app.run(function ($state) {
+    for (var i = views.length - 1; i >= 0; i--) {
+      var state = $state.get(views[i]);
+      if (state) {
+        state.templateUrl = 'app/auth/views/' + views[i] + '.html';
       }
-    }).state('signup', {
-      url: '/signup?referrer&r',
-      templateUrl: (views.signup ? 'app' : 'authApp') + '/auth/views/signup.html',
-      controller: 'SignupController',
-      controllerAs: 'vm',
-      parent: 'app'
-    }).state('password', {
-      url: '/password',
-      templateUrl: (views.password ? 'app' : 'authApp') + '/auth/views/password.html',
-      controller: 'PasswordController',
-      controllerAs: 'vm',
-      authenticate: true,
-      parent: 'app'
-    }).state('forgot', {
-      url: '/forgot',
-      templateUrl: (views.forgot ? 'app' : 'authApp') + '/auth/views/forgot.html',
-      controller: 'ForgotController',
-      controllerAs: 'vm',
-      parent: 'app'
-    }).state('reset', {
-      url: '/reset/:token',
-      templateUrl: (views.reset ? 'app' : 'authApp') + '/auth/views/reset.html',
-      controller: 'ResetController',
-      controllerAs: 'vm',
-      parent: 'app'
-    });
+    }
   });
 }
 
--- src/auth/index.js
+++ src/auth/index.js
@@ -26,54 +26,63 @@ require('./views/password.js');
 require('./views/reset.js');
 require('./views/signup.js');
 
+app.config(($stateProvider) => {
+  $stateProvider
+    .state('login', {
+      url: '/login?referrer',
+      templateUrl: 'authApp/auth/views/login.html',
+      controller: 'LoginController',
+      controllerAs: 'vm',
+      parent: 'app'
+    })
+    .state('logout', {
+      url: '/logout',
+      templateUrl: 'authApp/auth/views/login.html',
+      parent: 'app',
+      controller: ($state, Auth) => {
+        Auth.logout();
+        $state.go('login');
+      }
+    })
+    .state('signup', {
+      url: '/signup?referrer&r',
+      templateUrl: 'authApp/auth/views/signup.html',
+      controller: 'SignupController',
+      controllerAs: 'vm',
+      parent: 'app'
+    })
+    .state('password', {
+      url: '/password',
+      templateUrl: 'authApp/auth/views/password.html',
+      controller: 'PasswordController',
+      controllerAs: 'vm',
+      authenticate: true,
+      parent: 'app'
+    })
+    .state('forgot', {
+      url: '/forgot',
+      templateUrl: 'authApp/auth/views/forgot.html',
+      controller: 'ForgotController',
+      controllerAs: 'vm',
+      parent: 'app'
+    })
+    .state('reset', {
+      url: '/reset/:token',
+      templateUrl: 'authApp/auth/views/reset.html',
+      controller: 'ResetController',
+      controllerAs: 'vm',
+      parent: 'app'
+    });
+});
+
 function setOverwriteViews(views) {
-  app.config(($stateProvider) => {
-    $stateProvider
-      .state('login', {
-        url: '/login?referrer',
-        templateUrl: (views.login ? 'app' : 'authApp') + '/auth/views/login.html',
-        controller: 'LoginController',
-        controllerAs: 'vm',
-        parent: 'app'
-      })
-      .state('logout', {
-        url: '/logout',
-        templateUrl: 'app/auth/views/login.html',
-        parent: 'app',
-        controller: ($state, Auth) => {
-          Auth.logout();
-          $state.go('login');
-        }
-      })
-      .state('signup', {
-        url: '/signup?referrer&r',
-        templateUrl: (views.signup ? 'app' : 'authApp') + '/auth/views/signup.html',
-        controller: 'SignupController',
-        controllerAs: 'vm',
-        parent: 'app'
-      })
-      .state('password', {
-        url: '/password',
-        templateUrl: (views.password ? 'app' : 'authApp') + '/auth/views/password.html',
-        controller: 'PasswordController',
-        controllerAs: 'vm',
-        authenticate: true,
-        parent: 'app'
-      })
-      .state('forgot', {
-        url: '/forgot',
-        templateUrl: (views.forgot ? 'app' : 'authApp') + '/auth/views/forgot.html',
-        controller: 'ForgotController',
-        controllerAs: 'vm',
-        parent: 'app'
-      })
-      .state('reset', {
-        url: '/reset/:token',
-        templateUrl: (views.reset ? 'app' : 'authApp') + '/auth/views/reset.html',
-        controller: 'ResetController',
-        controllerAs: 'vm',
-        parent: 'app'
-      });
+  app.run(($state) => {
+    for (var i = views.length - 1; i >= 0; i--) {
+      var state = $state.get(views[i]);
+      if (state) {
+        state.templateUrl = 'app/auth/views/' + views[i] + '.html';
+      }
+    }
   });
 }
 
